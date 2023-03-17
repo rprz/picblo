c709a2473d885b2ffbb816b9942bb5941184e025
rprz <rpresh@gmail.com>
2023-03-17 19:54:54 -0400

    Create forms.py
    prompt:
    
     from app.forms import LoginForm, RegistrationForm, UploadForm
    ModuleNotFoundError: No module named 'app.forms'

---
+++ app/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, FileField
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
+from app.models import User
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=64)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+class UploadForm(FlaskForm):
+    image = FileField('Upload Image', validators=[DataRequired()])
+    submit = SubmitField('Upload')
