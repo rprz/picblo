a4c78976c7d171d072edf6ef921c2ea7774c27c3
rprz <rpresh@gmail.com>
2023-03-17 20:50:17 -0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -25,14 +25,16 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
+        if user is not None and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            next_page = request.args.get('next')
+            if not next_page or url_parse(next_page).netloc != '':
+                next_page = url_for('index')
+            logging.info(f'Successful login: {user.username}')
+            return redirect(next_page)
+        else:
             flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
+            logging.info('Failed login attempt')
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
@@ -51,7 +53,11 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
+        logging.info(f'Successful registration: {user.username}')
         return redirect(url_for('login'))
+    else:
+        if request.method == 'POST':
+            logging.info('Failed registration attempt')
     return render_template('register.html', title='Register', form=form)
 
 @app.route('/upload', methods=['GET', 'POST'])
