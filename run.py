f4eac1d7d70360dc81db475e3c868e53a4e5ffcd
rprz <rpresh@gmail.com>
2023-03-17 20:05:00 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
-from app import app
+from app import app, db
+from app.models import User, Picture
+
+def create_db():
+    db.create_all()
 
 if __name__ == '__main__':
+    create_db()
     app.run()
