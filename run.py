0a659fb775dcbe8023a640311ddaa8cde2695eea
rprz <rpresh@gmail.com>
2023-03-17 20:10:51 -0400

    Update run.py

--- run.py
+++ run.py
@@ -2,11 +2,11 @@ from app import app, db
 from app.models import User, Picture
 
 def init_db():
-    db.create_all()
+    with app.app_context():
+        db.create_all()
 
     # Add any other initialization code here
 
 if __name__ == '__main__':
     init_db()
     app.run()
-    
